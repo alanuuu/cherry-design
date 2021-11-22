41e89e932b1f8fb4767f2ae7c10565f49ba5c02e
zouxin1 <zouxin1@csdn.net>
2021-11-22 17:41:43 +0800

    chore: 构建初始化

---
+++ packages/vue-ui/components/button/button.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="c-btn">
+    <button>{{slot}}</button>
+  </div>
+</template>
+
+<script>
+/**
+ * 按钮类型
+ */
+export const btnTypes = [
+  'primary',
+  'ghost',
+  'light'
+];
+/**
+ * 按钮状态
+ */
+export const btnStatus = [
+  'normal',
+  'warning',
+  'danger'
+];
+
+export default {
+  name: "Button",
+  props: {
+    type: {
+      type: String,
+      default: 'primary',
+      validator: (val) => {
+        return btnTypes.includes(val)
+      }
+    },
+    status: {
+      type: String,
+      default: 'normal',
+      validator: (val) => {
+        return btnStatus.includes(val)
+      }
+    }
+  },
+  computed: {
+    cls() {
+    
+    }
+  }
+}
+</script>
+
+<style>
+</style>
