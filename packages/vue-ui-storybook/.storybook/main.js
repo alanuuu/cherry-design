bad47a7e34932f8d2f883d7ba673dec42bc1ddd2
zouxin1 <zouxin1@csdn.net>
2021-11-29 15:34:17 +0800

    feat(storybook): add less

--- packages/vue-ui-storybook/.storybook/main.js
+++ packages/vue-ui-storybook/.storybook/main.js
@@ -30,7 +30,9 @@ module.exports = {
         {
           loader: require.resolve("less-loader"),
           options: {
-            javascriptEnabled: true,
+            lessOptions: {
+              javascriptEnabled: true,
+            }
           },
         },
       ],
