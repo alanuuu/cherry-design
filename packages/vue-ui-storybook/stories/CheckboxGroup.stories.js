eb7c9f8d8f4129452633be1d8239aa8939873ee2
ouyw <ouyw@csdn.net>
2021-12-05 14:26:06 +0800

    perf：优化checkbox组件

--- packages/vue-ui-storybook/stories/CheckboxGroup.stories.js
+++ packages/vue-ui-storybook/stories/CheckboxGroup.stories.js
@@ -1,11 +1,62 @@
 import { Checkbox, CheckboxGroup } from '@csdn-design/vue-ui';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: '组件/CheckboxGroup 复选框',
   component: { CheckboxGroup, Checkbox },
+  args: {
+    size: 'medium',
+    checked: ['1'],
+    disabled: false,
+    border: false,
+    list: [
+      {
+        value: '1',
+        label: '苹果',
+      },
+      {
+        value: '2',
+        label: '西瓜',
+      },
+    ],
+  },
   argTypes: {
     size: {
-      control: { type: 'select', options: ['small', 'medium', 'large'] },
+      control: { type: 'radio' },
+      options: ['large', 'medium', 'small'],
+      description: '复选框大小',
+      table: {
+        defaultValue: {
+          summary: 'medium',
+        },
+      },
+    },
+    list: {
+      control: { type: 'array' },
+      description: '选择列表',
+      table: {
+        defaultValue: {
+          summary: '[]',
+        },
+      },
+    },
+    checked: {
+      control: { type: 'array' },
+      description: '选中',
+      table: {
+        defaultValue: {
+          summary: '[]',
+        },
+      },
+    },
+    border: {
+      control: { type: 'boolean' },
+      description: '是否有边框',
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
     },
   },
 };
@@ -15,25 +66,11 @@ const Template = (args, { argTypes }) => ({
   components: { CheckboxGroup, Checkbox },
   template:
     '<div><checkbox-group @change="change" :size="size" v-model="checked"><checkbox :border="border" :disabled="disabled" v-for="(item, index) in list" :key="index" :label="item.value">{{item.label}}</checkbox></checkbox-group></div>',
+  methods: {
+    change: action('change'),
+  },
 });
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  disabled: false,
-  border: false,
-  checked: ['1'],
-  list: [
-    {
-      value: '1',
-      label: '苹果',
-    },
-    {
-      value: '2',
-      label: '西瓜',
-    },
-  ],
-  change() {
-    console.log('change');
-  },
-};
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {};
