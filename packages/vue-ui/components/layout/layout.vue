dd310065f64e04e3300727557b21bcc0a19eb58f
zouxin1 <zouxin1@csdn.net>
2021-12-06 14:01:34 +0800

    feat(component): add layout

--- packages/vue-ui/components/layout/layout.vue
+++ packages/vue-ui/components/layout/layout.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="c-layout">
-    layout
-  </div>
+  <section class="c-layout" :class="cls">
+    <slot></slot>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'Layout',
+  props: {
+    // 一般不使用、主要用于解决服务端渲染闪烁
+    hasSider: {
+      type: Boolean,
+    },
+  },
+  data() {
+    return {
+      siders: [],
+    };
+  },
+  provide() {
+    return {
+      onSiderCreate: this.onSiderCreate,
+    };
+  },
+  methods: {
+    onSiderCreate(id) {
+      this.siders.push(id);
+    },
+  },
+  computed: {
+    cls() {
+      return {
+        'c-has-sider': this.hasSider || this.siders.length > 0,
+      };
+    },
+  },
 };
 </script>
---
+++ packages/vue-ui/components/layout/sider.vue
@@ -0,0 +1,29 @@
+<template>
+  <aside class="c-layout-sider">
+    <div class="c-layout-sider-inner">
+      <slot></slot>
+    </div>
+  </aside>
+</template>
+
+<script>
+const genId = ((key) => {
+  let i = 0;
+  // eslint-disable-next-line func-names
+  return function () {
+    i += 1;
+    return key + i;
+  };
+})();
+
+const key = 'C_LAYOUT_SIDER';
+
+export default {
+  name: 'LayoutSider',
+  inject: ['onSiderCreate'],
+  mounted() {
+    const id = genId(key);
+    this.onSiderCreate(id);
+  },
+};
+</script>
