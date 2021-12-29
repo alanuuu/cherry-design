be835d162bd3b5a0565b1dce8238ec31dbe0ae8c
zouxin1 <zouxin1@csdn.net>
2021-12-29 16:14:00 +0800

    feat: init upload component

---
+++ packages/vue-ui-storybook/stories/Upload.stories.js
@@ -0,0 +1,40 @@
+export default {
+  title: '组件/Tag 标签',
+  args: {
+    size: 'medium',
+    circle: false,
+    theme: 'normal',
+    active: false,
+  },
+  argTypes: {
+    size: {
+      control: { type: 'radio' },
+      options: ['small', 'medium', 'large'],
+    },
+    theme: {
+      control: { type: 'radio', defaultValue: 'normal' },
+      options: ['normal', 'light'],
+    },
+    circle: {
+      control: { type: 'boolean' },
+    },
+    active: {
+      control: { type: 'boolean' },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  template: '<c-tag v-bind="$props">标签</c-tag>',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+
+};
+
+export const Link = Template.bind({});
+Link.args = {
+  link: 'www.csdn.net',
+};
