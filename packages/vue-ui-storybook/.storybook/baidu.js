e3a7bbf08dbff3025588e3f186dc7018b024e8f7
alanuuu <alanuuu@163.com>
2022-05-19 19:37:41 +0800

    chore: add baidujs

---
+++ packages/vue-ui-storybook/.storybook/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?41550ca00f8ac488a86182ca4a9ec8a2';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
